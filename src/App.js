016dce51ce6065c138af790b59680acc7a6d4ea6
rogu2 <rguempel@gmail.com>
2019-08-28 14:37:08 -0400

    add materialize, remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, Fragment } from 'react';
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css/dist/js/materialize.min.js'
+import Search from './components/layout/Search'
 import './App.css';
 
-function App() {
+const App =() => {
+
+  // initialize materialize js
+  useEffect(() => {
+    M.AutoInit()
+  })
   return (
-    <div className="App">
-    </div>
+    <Fragment>
+      <Search />
+    </Fragment>
   );
 }
 
